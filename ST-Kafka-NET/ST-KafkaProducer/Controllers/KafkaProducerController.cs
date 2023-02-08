9642fc6578386597a4ac0832d7695afb456b3019
Yass15 <yassine.abdelkefi@enis.tn>
2023-02-08 10:14:34 +0100

    first commit

---
+++ ST-Kafka-NET/ST-KafkaProducer/Controllers/KafkaProducerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+namespace ST_KafkaProducer.Controllers
+{
+    [Route("api/kafka")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig
+        { BootstrapServers = "localhost:9092" };
+        private readonly string topic = "simpletalk_topic";
+
+        [HttpPost("sendNotif")]
+        public async Task<IActionResult> Post([FromBody] string message)
+        {
+            return Created(string.Empty, await SendToKafka(topic, message));
+        }
+        private async Task<object> SendToKafka(string topic, string message)
+        {
+            using (var producer =
+                 new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    return await producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            return null;
+        }
+    }
+}
